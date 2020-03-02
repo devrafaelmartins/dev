cfc5f64600ff75d1dbf50591bdb1b180ca6b16a9
Rafael Martins <engenheirorafaelmartins@gmail.com>
2020-03-02 09:04:34 -0300

    Entidade user e Banco H2

---
+++ src/main/java/com/example/workspace/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.example.workspace.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.workspace.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Rafael", "dev@gmail.com", "99999-9999", "321654");
+		return ResponseEntity.ok().body(u);
+	}
+}
